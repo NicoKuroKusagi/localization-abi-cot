836832402527e988197e1389585171abb5f41eef
Leon Kükel <verwaltung@unit13.de>
2021-11-06 23:57:58 +0100

    Create de.php

---
+++ de.php
@@ -0,0 +1,297 @@
+<?php
+
+/*
+	Boot
+*/
+define("COT_UI_BOOT_STARTING_UP", "Community Online Tools startet");
+define("COT_UI_BOOT_STARTING_UP_TIMETAKE", "Dies dauert in der Regel wenige Minuten.");
+define("COT_UI_BOOT_STARTING_UP_FAQ1_HEAD", "Warum ist COT nicht verfügbar?");
+define("COT_UI_BOOT_STARTING_UP_FAQ1_TEXT", "Wir starten die Server neu um Aktualisierungen anzuwenden und die Funktion von COT sicherzustellen.");
+define("COT_UI_BOOT_STARTING_UP_FAQ2_HEAD", "Wann ist es wieder verfügbar?");
+define("COT_UI_BOOT_STARTING_UP_FAQ2_TEXT", "Das starten von COT dauert nur wenige Minuten. Hol dir einen Tee und warte, wir sind sofort wieder da.");
+define("COT_UI_BOOT_STARTING_UP_FAQ3_HEAD", "Du hast weitere Fragen?");
+define("COT_UI_BOOT_STARTING_UP_FAQ3_TEXT", "Falls diese Seite über längere Zeit sichtbar bleibt, schreib uns an noc@abinteractive.net.");
+
+/*
+	Access
+*/
+define("COT_UI_ACCESS_UNAUTHORIZED", "Nicht autorisiert");
+define("COT_UI_ACCESS_UNAUTHORIZED_LONG", "Du bist nicht autorisiert, diese Seite anzuzeigen. Der Versuch diese Seite anzuzeigen wurde in unseren Logs dokumentiert.");
+
+/*
+	Universal
+*/
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_PUBLIC", "Öffentlich");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_FRIENDSOFFRIENDS", "Freunde von Freunden");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_FRIENDS", "Freunde");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_GROUP", "Gruppe");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_EVENT", "Event");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_OWNERMUSTINVITE", "Inhaber muss einladen");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_EVERYONECANINVITE", "Jeder kann einladen");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_DEVELOPER", "Nur-Entwickler instanz");
+define("COT_UI_UNIVERSAL_PRIVACY_STATE_MATCHMAKING", "Matchmaking instanz");
+
+define("COT_UI_UNIVERSAL_COUNTRY_US", "Vereinigte Staaten");
+define("COT_UI_UNIVERSAL_COUNTRY_AS", "Singapur");
+define("COT_UI_UNIVERSAL_COUNTRY_DE", "Deutschland");
+
+define("COT_UI_UNIVERSAL_TEXT_PLACEHOLDER_REQUIRED", "Benötigt");
+
+define("COT_UI_UNIVERSAL_TEXT_LIST_EMPTY", "Diese Liste ist zur Zeit leer.");
+define("COT_UI_UNIVERSAL_TEXT_ERROR", "Fehler");
+define("COT_UI_UNIVERSAL_TEXT_RESPONSE_REQUEST_PROCESSED", "Anfrage verarbeitet");
+define("COT_UI_UNIVERSAL_TEXT_RESPONSE_FAILED_TO_COMPLETE_REQUEST", "Fehler bei der Verarbeitung der Anfrage");
+define("COT_UI_UNIVERSAL_TEXT_SELECTION_REQUIRED", "Auswahl benötigt");
+
+define("COT_UI_UNIVERSAL_TEXT_LOADING_PAGE", "Seite wird geladen");
+
+define("COT_UI_UNIVERSAL_BUTTON_SAVE_SETTINGS", "Einstellungen speichern");
+define("COT_UI_UNIVERSAL_BUTTON_OK", "Okay");
+define("COT_UI_UNIVERSAL_BUTTON_YES", "Ja");
+define("COT_UI_UNIVERSAL_BUTTON_NO", "Nein");
+define("COT_UI_UNIVERSAL_BUTTON_CANCEL", "Abbruch");
+define("COT_UI_UNIVERSAL_BUTTON_SURE", "Ich bin mir sicher");
+define("COT_UI_UNIVERSAL_BUTTON_ACCEPT", "Annehmen");
+define("COT_UI_UNIVERSAL_BUTTON_REJECT", "Ablehnen");
+
+define("COT_UI_UNIVERSAL_RANK_NONE", "Kein Zugang");
+define("COT_UI_UNIVERSAL_RANK_VIEWER", "Nur-Lesen-Zugang");
+define("COT_UI_UNIVERSAL_RANK_MOD", "Moderator");
+define("COT_UI_UNIVERSAL_RANK_ADMIN", "Administrator");
+define("COT_UI_UNIVERSAL_RANK_SUPERVISOR", "Aufsichtsrat");
+define("COT_UI_UNIVERSAL_RANK_HOSTMASTER", "Hostmaster");
+define("COT_UI_UNIVERSAL_RANK_OWNER", "Inhaber");
+
+define("COT_UI_UNIVERSAL_SAVED_SETTINGS", "Deine Einstellungen wurden übernommen.");
+
+define("COT_UI_UNIVERSAL_BUTTON_SIGN_OUT", "Abmelden");
+
+/*
+	Navigation
+*/
+define("COT_UI_NAV_SIDEBAR_DASHBOARD", "Kontrollzentrum");
+define("COT_UI_NAV_SIDEBAR_SERVERS", "Server");
+define("COT_UI_NAV_SIDEBAR_TEAMS", "Teams");
+define("COT_UI_NAV_SIDEBAR_BANLISTS", "Ausschlusslisten");
+define("COT_UI_NAV_SIDEBAR_HIVES", "Hives & Verbindungen");
+define("COT_UI_NAV_SIDEBAR_STATUS_PAGES", "Status-Seiten");
+define("COT_UI_NAV_SIDEBAR_MODERATION", "Moderation");
+define("COT_UI_NAV_SIDEBAR_AUDIT", "Audit-Log");
+define("COT_UI_NAV_SIDEBAR_BILLING", "Bezahlsystem");
+define("COT_UI_NAV_SIDEBAR_SETTINGS", "Konto-Einstellungen");
+define("COT_UI_NAV_SIDEBAR_SUPPORT", "Hilfe erhalten");
+define("COT_UI_NAV_SIDEBAR_SYSTEM", "Systemeinstellungen");
+
+define("COT_UI_NAV_SIDEBAR_MODERATION_PROFILE", "Moderationsprofil");
+
+define("COT_UI_NAV_WELCOME_BACK", "Willkommen zurück, {usr}!");
+
+/*
+	Dashboard
+*/
+define("COT_UI_DASH_HEAD", "Kontrollzentrum");
+define("COT_UI_DASH_SERVERS_ASSIGNED", "Server zugewiesen");
+define("COT_UI_DASH_HIVES_ASSIGNED", "Hives zugewiesen");
+define("COT_UI_DASH_GUARDIAN_INCIDENTS", "Wächter-Vorfälle");
+define("COT_UI_DASH_WATCHDOG_ALERTS", "Verfügbarkeits-Vorfälle");
+
+define("COT_UI_DASH_WIDGET_CCU_HEAD", "Gleichzeitige Benutzer auf deinen Servern");
+define("COT_UI_DASH_WIDGET_CCU_HOUR", "vor einer Stunde");
+define("COT_UI_DASH_WIDGET_CCU_DAY", "vor einem Tag");
+define("COT_UI_DASH_WIDGET_CCU_WEEK", "vor einer Woche");
+
+/*
+	Servers List
+*/
+define("COT_UI_SERVERS_HEAD", "Server");
+define("COT_UI_SERVERS_BUTTON_MANAGE", "Verwalten");
+
+define("COT_UI_SERVERS_CREATE_SERVER_HEAD", "Einen neuen Server anlegen");
+define("COT_UI_SERVERS_CREATE_SERVER_TEXT", "Durch Betätigung des Buttons wird der Assistent für die Erstellung von Servern gestartet, der Dir dabei helfen wird einen Server zu erstellen und dem richtigen Team zuzuweisen.");
+define("COT_UI_SERVERS_BUTTON_CREATE_SERVER", "Server erstellen");
+
+define("COT_UI_SERVERS_TEXT_NO_SERVERS_FOR_TEAM", "Dieses Team vererbt dir momentan keine Zugänge zu reservierten Instanzen.");
+
+define("COT_UI_SERVERS_BUTTON_CREATE_SERVER_NAME", "Server name");
+define("COT_UI_SERVERS_BUTTON_CREATE_WORLD_ID", "Welt-ID");
+define("COT_UI_SERVERS_BUTTON_CREATE_SELECT_PRIVACY", "Eine Privatsphäre-Einstellung wählen");
+define("COT_UI_SERVERS_BUTTON_CREATE_SELECT_TEAM", "Ein Team auswählen");
+define("COT_UI_SERVERS_BUTTON_CREATE_SELECT_REGION", "Eine Region auswählen");
+
+/*
+	Server Details Page
+*/
+define("COT_UI_SERVER_DETAIL_HEAD", "Server-Verwalter");
+define("COT_UI_SERVER_DETAIL_SERVER_VERSION_HEAD", "Server version");
+define("COT_UI_SERVER_DETAIL_GUARDIAN_HEAD", "Wächter");
+define("COT_UI_SERVER_DETAIL_MODULES_HEAD", "Aktivierte Module");
+define("COT_UI_SERVER_DETAIL_MANAGEMENT_FEATURES_HEAD", "Verwaltungs-Funktionen");
+define("COT_UI_SERVER_DETAIL_MISSION_FEATURES_HEAD", "Missionssystem-Funktionen");
+define("COT_UI_SERVER_DETAIL_GUARDIAN_FEATURES_HEAD", "Wächter-Funktionen");
+define("COT_UI_SERVER_DETAIL_GUARDIAN_CONSOLE_HEAD", "Wächter Fernkonsole");
+define("COT_UI_SERVER_DETAIL_RECENT_EVENTS_LOG_HEAD", "Letzte Vorfälle");
+
+define("COT_UI_SERVER_DETAIL_GUARDIAN_TEXT", "Wächter-Modus ist aktiviert.
+Betrügen führt zu einer globalen, dauerhaften Sperrung die von jedem Server der die Wächter-Ausschlussliste verwendet übernommen wird. Für weitere Informationen, schau bitte in die Dokumentation.");
+
+define ("COT_UI_SERVER_DETAIL_MODULE_PLAYERS_ONLINE", "Spieler online");
+
+define ("COT_UI_SERVER_DETAIL_BUTTON_START", "Server starten");
+define ("COT_UI_SERVER_DETAIL_BUTTON_STOP", "Server herunterfahren");
+define ("COT_UI_SERVER_DETAIL_BUTTON_RESTART", "Server neu starten");
+define ("COT_UI_SERVER_DETAIL_BUTTON_REBUILD", "Neu erstellen");
+define ("COT_UI_SERVER_DETAIL_BUTTON_DESTROY", "Instanz zerstören");
+
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_GUARDIAN_TIMELINE_HEAD", "Wächter Bionic Timeline");
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_GUARDIAN_CONSOLE_HEAD", "Wächter Fernkonsole");
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_PLAYER_MAP_HEAD", "Spieler-Karte");
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_PLAYERS_CONNECTED_HEAD", "Verbundene Spieler");
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_SETTINGS_HEAD", "Server-Einstellungen");
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_MISSION_UPLOAD", "Missionsdatei hochladen");
+define ("COT_UI_SERVER_DETAIL_MODULARWINDOW_COMPLIANCE_HEAD", "Compliance");
+
+/*
+	Server Management Texts
+*/
+define("COT_UI_SERVER_MANAGE_STARTED", "Der Server wurde gestartet.");
+define("COT_UI_SERVER_MANAGE_STOPPED", "Der Server wurde gestoppt.");
+define("COT_UI_SERVER_MANAGE_RESTARTED", "Der Server wurde neu gestartet.");
+define("COT_UI_SERVER_MANAGE_DESTROYED", "Der Server wurde gelöscht.");
+
+/*
+	Teams Texts
+*/
+define("COT_UI_TEAMS_HEAD", "Teams");
+
+define("COT_UI_TEAMS_CREATE_TEAM_HEAD", "Erstelle ein Team");
+define("COT_UI_TEAMS_CREATE_TEAM_TEXT", "Wenn der folgende Button betätigt wird, wird ein neues Team angefragt. Wenn diese Aktion Deine Limits nicht überschreitet, wird das Team sofort erstellt.");
+define("COT_UI_TEAMS_JOIN_TEAM_HEAD", "Einem Team beitreten");
+define("COT_UI_TEAMS_JOIN_TEAM_TEXT", "Diese Funktion erlaubt dir eine Beitritts-Anfrage an ein Team zu senden. Ein berechtigter Teamleiter ist dann in der Lage dich anzunehmen oder abzulehnen.");
+define("COT_UI_TEAMS_JOIN_TEAM_WARNING_NO_TEAM", "Um eine Beitrittsanfrage an ein Team zu senden ist die Eingabe einer Team-ID erforderlich.");
+
+define("COT_UI_TEAMS_JOIN_REQUEST_SENT", "Du hast erfolgreich angefragt, dem Team {team} beizutreten.");
+define("COT_UI_TEAMS_JOIN_REQUEST_NO_SUCH_TEAM", "{team} existiert nicht.");
+define("COT_UI_TEAMS_JOIN_REQUEST_NO_JOIN_REQUESTS", "{team} nimmt keine Beitritts-Anfragen an.");
+define("COT_UI_TEAMS_JOIN_REQUEST_ALREADY_IN_TEAM", "Du bist bereits Mitglied von {team}.");
+
+define("COT_UI_TEAMS_TOO_MANY_TEAMS_TEXT", "Du hast die maximale Anzahl an teams die ein Benutzer erstellen kann erreicht. Du kannst ein Team löschen oder an einen anderen Benutzer übergeben um ein neues erstellen zu können.");
+define("COT_UI_TEAMS_TEAM_CREATED_TEXT", "Das neue Team {team} wurde erstellt. Du kannst es nun auf der Verwaltungs-Seite des Teams bearbeiten.");
+define("COT_UI_TEAMS_TEAM_DELETED_TEXT", "Das Team {team} wurde gelöscht.");
+
+define("COT_UI_TEAMS_BUTTON_CREATE_TEAM", "Team erstellen");
+define("COT_UI_TEAMS_BUTTON_JOIN_TEAM", "Beitritts-Anfrage senden");
+
+/*
+	Teams Details Page
+*/
+define("COT_UI_TEAMS_DETAIL_HEAD", "Team verwalten");
+define("COT_UI_TEAMS_DETAIL_PILLBOARD_HEAD", "{team} verwalten");
+
+define("COT_UI_TEAMS_DETAIL_TAB_STATS", "Statistiken");
+define("COT_UI_TEAMS_DETAIL_TAB_JOIN_REQUESTS", "Beitritts-Anfragen");
+define("COT_UI_TEAMS_DETAIL_TAB_MEMBERS", "Mitglieder");
+define("COT_UI_TEAMS_DETAIL_TAB_CONNECTED_INSTANCES", "Verbundene Server");
+define("COT_UI_TEAMS_DETAIL_TAB_CONNECTED_BANLISTS", "Verbundene Ausschlusslisten");
+define("COT_UI_TEAMS_DETAIL_TAB_CONNECTED_HIVES", "Verbundene Hives");
+define("COT_UI_TEAMS_DETAIL_TAB_STATUS_PAGES", "Verbundene Status-Seiten");
+define("COT_UI_TEAMS_DETAIL_TAB_SETTINGS", "Einstellungen");
+
+define("COT_UI_TEAMS_SETTINGS_SAVE_HEADER", "Alle Einstellungs-Änderungen übernehmen");
+define("COT_UI_TEAMS_SETTINGS_SAVE_TEXT", "Wende alle oben getätigten Einstellungen an, falls deine Berechtigungen innerhalb des Teams dies zulassen.");
+define("COT_UI_TEAMS_SETTINGS_SAVE_BUTTON", "Änderungen übernehmen");
+
+define("COT_UI_TEAMS_SETTINGS_SUB_GENERAL", "Allgemeine Einstellungen");
+define("COT_UI_TEAMS_SETTINGS_SUB_PERMISSIONS", "Berechtigungs-System");
+define("COT_UI_TEAMS_SETTINGS_SUB_PRIVACY", "Privatsphäre Einstellungen");
+
+define("COT_UI_TEAMS_SETTINGS_TEAM_ID", "Team-ID");
+define("COT_UI_TEAMS_SETTINGS_TEAM_NAME", "Team Name");
+define("COT_UI_TEAMS_SETTINGS_TEAM_DESCRIPTION", "Team Beschreibung");
+
+define("COT_UI_TEAMS_PERMISSIONS_INHERIT_HINT", "Beim zuweisen von Rechten zu Gruppen bedenke bitte, dass wir dieses recht auch allen Gruppen vererben, die wir als höher ansehen. Wenn du das Recht zum bearbeiten von Ausschlusslisten an die Moderatoren vergibst, können auch alle Rollen über der Moderatoren-Rolle dieses Recht nutzen. Bei Eingaben die wir nicht validieren können, gehen wir davon aus, dass das Recht nur für den Inhaber der Gruppe bestimmt ist. Der Inhaber des Teams hat per definition immer alle Rechte.");
+
+define("COT_UI_TEAMS_PERMISSIONS_HEADER_MEMBERS", "Mitglieder");
+define("COT_UI_TEAMS_PERMISSIONS_MEMBERS_MANAGE", "Mitglieder verwalten");
+
+define("COT_UI_TEAMS_PERMISSIONS_STATUS_PAGES_HEADER", "Status-Seiten");
+define("COT_UI_TEAMS_PERMISSIONS_STATUS_PAGES_CREATE", "Status-Seiten erstellen");
+define("COT_UI_TEAMS_PERMISSIONS_STATUS_PAGES_MANAGE", "Status-Seiten verwalten");
+define("COT_UI_TEAMS_PERMISSIONS_STATUS_PAGES_DELETE", "Status-Seiten löschen");
+
+define("COT_UI_TEAMS_PERMISSIONS_HEADER_SERVERS", "Server");
+define("COT_UI_TEAMS_PERMISSIONS_SERVERS_EDIT", "Server bearbeiten");
+define("COT_UI_TEAMS_PERMISSIONS_SERVERS_OPERATE", "Server bedienen");
+define("COT_UI_TEAMS_PERMISSIONS_SERVERS_CREATE", "Server erstellen");
+define("COT_UI_TEAMS_PERMISSIONS_SERVERS_DELETE", "Server löschen");
+
+define("COT_UI_TEAMS_PERMISSIONS_HEADER_BANLISTS", "Ausschlusslisten");
+define("COT_UI_TEAMS_PERMISSIONS_BANLISTS_EDIT", "Ausschlusslisten bearbeiten");
+define("COT_UI_TEAMS_PERMISSIONS_BANLISTS_MODERATION", "Ausschlusslisten moderation");
+define("COT_UI_TEAMS_PERMISSIONS_BANLISTS_CREATE", "Ausschlusslisten erstellen");
+define("COT_UI_TEAMS_PERMISSIONS_BANLISTS_DELETE", "Ausschlusslisten löschen");
+
+define("COT_UI_TEAMS_PERMISSIONS_HEADER_HIVES", "Hives");
+define("COT_UI_TEAMS_PERMISSIONS_HIVES_EDIT", "Hives bearbeiten");
+define("COT_UI_TEAMS_PERMISSIONS_HIVES_CREATE", "Hives erstellen");
+define("COT_UI_TEAMS_PERMISSIONS_HIVES_DELETE", "Hives löschen");
+
+define("COT_UI_TEAMS_PRIVACY_ACCEPT_JOIN_REQUESTS", "Akzeptanz von Beitritts-Anfragen");
+
+/*
+	Banlists Texts
+*/
+define("COT_UI_BANLISTS_HEAD", "Ausschlusslisten");
+define("COT_UI_BANLISTS_CREATE_BANLIST_HEAD", "Erstelle eine Ausschlussliste");
+define("COT_UI_BANLISTS_CREATE_BANLIST_TEXT", "Wenn der folgende Button betätigt wird, wird eine neue Ausschlussliste angefragt. Wenn diese Aktion Deine Limits nicht überschreitet, wird die Ausschlussliste sofort erstellt.");
+
+define("COT_UI_BANLISTS_CREATE_BANLIST_WARNING_NO_TEAM", "Um eine Ausschlussliste anzulegen musst du ein Team auswählen.");
+
+define("COT_UI_BANLISTS_TOO_MANY_BANLISTS_TEXT", "Du hast die maximale Anzahl an Ausschlusslisten die ein Benutzer erstellen kann erreicht. Du kannst eine Ausschlussliste löschen oder an einen anderen Benutzer übergeben um eine neue erstellen zu können.");
+define("COT_UI_BANLISTS_BANLIST_CREATED_TEXT", "Die neue Ausschlussliste {banlist} wurde erstellt. Du kannst sie nun auf der Verwaltungs-Seite der Ausschlussliste bearbeiten.");
+define("COT_UI_BANLISTS_BANLIST_DELETED_TEXT", "Die Ausschlussliste {banlist} wurde gelöscht.");
+
+define("COT_UI_BANLISTS_BUTTON_CREATE_BANLIST", "Ausschlussliste erstellen");
+
+/*
+	Banlist Details Page
+*/
+define("COT_UI_BANLISTS_DETAIL_HEAD", "Ausschlussliste verwalten");
+define("COT_UI_BANLISTS_DETAIL_PILLBOARD_HEAD", "{banlist} verwalten");
+
+define("COT_UI_BANLISTS_DETAIL_TAB_STATS", "Statistiken");
+define("COT_UI_BANLISTS_DETAIL_TAB_DATABASE", "Datenbank");
+define("COT_UI_BANLISTS_DETAIL_TAB_BAN_APPEALS", "Entbannungs-Anträge");
+define("COT_UI_BANLISTS_DETAIL_TAB_TEAMS", "Verbundene Teams");
+define("COT_UI_BANLISTS_DETAIL_TAB_SUBSCRIBERS", "Abonnenten");
+define("COT_UI_BANLISTS_DETAIL_TAB_SETTINGS", "Einstellungen");
+
+define("COT_UI_BANLISTS_DETAIL_MODAL_CREATE_BAN", "Einen Spieler ausschließen");
+
+define("COT_UI_BANLISTS_DETAIL_DB_BAN_ID", "Ausschluss-ID");
+define("COT_UI_BANLISTS_DETAIL_DB_USER", "Benutzer");
+define("COT_UI_BANLISTS_DETAIL_DB_REASON", "Begründung");
+define("COT_UI_BANLISTS_DETAIL_DB_UNTIL", "Ausgeschlossen bis");
+define("COT_UI_BANLISTS_DETAIL_DB_MODERATOR", "Moderator");
+define("COT_UI_BANLISTS_DETAIL_DB_CONTROLS", "Steuerung");
+
+define("COT_UI_BANLISTS_DETAIL_BUTTON_CREATE_BAN", "Ausschluss erstellen");
+define("COT_UI_BANLISTS_DETAIL_BUTTON_IMPORT_BAN", "Von Ausschlussliste importieren");
+
+/*
+	User-Settings Page
+*/
+define("COT_UI_SETTINGS_HEAD", "Konto-Einstellungen");
+define("COT_UI_SETTINGS_BOX_HEAD", "Benutzerkonten-Steuerung");
+define("COT_UI_SETTINGS_TEXT_PLATFORM_ACCOUNT", "Verbunderes Plattform-Konto");
+define("COT_UI_SETTINGS_TEXT_RI_TOKENS", "Reservierte Instanz Tokens");
+define("COT_UI_SETTINGS_TEXT_RI_TOKENS_EXPLAINATION", "Reservierte Instanz Tokens können zu Teams übertragen werden. Dort werden sie als Zahlungsmittel für Speicherplatz und Instanzen verwendet.");
+define("COT_UI_SETTINGS_TEXT_RI_TOKENS_SHORT", "RI Tokens");
+define("COT_UI_SETTINGS_TEXT_LIMITS_MAX_TEAMS", "Maximale Anzahl von Teams im Besitz");
+define("COT_UI_SETTINGS_TEXT_LIMITS_MAX_BANLISTS", "Maximale Anzahl von Ausschlusslisten pro Team");
+define("COT_UI_SETTINGS_TEXT_LIMITS_MAX_HIVES", "Maximale Anzahl von Hives im Besitz");
+define("COT_UI_SETTINGS_TEXT_LIMITS_MAX_RESERVED_INSTANCES", "Maximale anzahl von reservierten Instanzen im Besitz");
+
+define("COT_UI_SETTINGS_TEXT_LANGUAGE_SELECT", "Eine Sprache auswählen");
+define("COT_UI_SETTINGS_TEXT_LANGUAGE_SELECT_NOTE", "Standardmäßig verwenden wir die Sprache, die der Internet-Browser des Endbenutzers anfragt. Um eine andere Sprache festzulegen, ändere bitte diese Einstellung.");
+
+define("COT_UI_SETTINGS_BUTTON_DARKMODE", "Darstellung im Dunkelmodus");
